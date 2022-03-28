d002096b4a932e6ce79c16b828be3f965298dbde
Ibejih <chukaibejih@gmail.com>
2022-03-28 03:21:24 +0100

    initial commit

---
+++ accounts/models.py
@@ -0,0 +1,90 @@
+import uuid
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.db.models.signals import post_save
+from django.db import models
+from django.dispatch import receiver
+
+# Create your models here.
+
+
+class CustomUserManager(UserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, username, password=None, **extra_fields):
+        
+        if not email:
+            raise ValueError("Set an email address")
+        if not username:
+            raise ValueError("Set a username")
+        if not password:
+            raise ValueError("Set a password")
+        
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+            **extra_fields
+            )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, username, password, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, username, password, **extra_fields)
+        
+    def create_superuser(self, email, username, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self._create_user(email, username, password, **extra_fields)
+        
+
+class User(AbstractUser):
+    """
+    A custom user of the eRecipe app.
+
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_organization = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+    
+    def __str__(self):
+        return self.username    
+
+
+class UserProfile(models.Model):
+    """
+    A user profile of the eRecipe app.
+
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    bio = models.TextField(max_length=500, blank=True)
+    avatar = models.ImageField(upload_to="avatars/", blank=True)
+    social_links = models.JSONField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.email
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+
+
+
